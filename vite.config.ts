52b1412bf0beb08517cd21331f187e62e67c7bcb
常伴你左右 <hello2000@vip.qq.com>
2022-12-05 11:20:18 +0800

    remove turbo

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,6 @@ export default defineConfig(({ mode }) => {
             outDir: 'lib',
             lib: {
                 entry: resolve(__dirname, 'packages/index.ts'),
-                formats: ['es', 'esm', 'umd', 'cjs'],
                 name: 'alwaysme',
                 fileName: 'index'
             },
