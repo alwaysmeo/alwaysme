9d542b45619018d37439739cb7a905bb49832d63
常伴你左右 <hello2000@vip.qq.com>
2022-12-06 17:20:18 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,25 @@
 module.exports = {
 	env: {
 		browser: true,
-		es2021: true,
+		es2021: true
 	},
-	extends: [
-		'eslint:recommended',
-		'plugin:vue/vue3-essential',
-		'plugin:@typescript-eslint/recommended',
-	],
+	extends: ['eslint:recommended', 'plugin:vue/vue3-essential', 'plugin:@typescript-eslint/recommended'],
 	overrides: [],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
-		sourceType: 'module',
+		sourceType: 'module'
 	},
 	plugins: ['vue', '@typescript-eslint'],
 	rules: {
-		indent: ['error', 'tab', { SwitchCase: 1 }],
-	},
-};
+		indent: ['error', 'tab', { allowSingleLine: true }], // 强制使用制表符缩进
+		'comma-dangle': ['warn', 'never'], // 对象字面量项尾不能有逗号
+		'comma-spacing': ['error', { before: false, after: true }], // 控制逗号前后的空格
+		'keyword-spacing': ['error', { before: true, after: true }], // 强制在关键字前后使用一致的空格
+		'no-const-assign': 'error', // 禁止修改 const 声明的变量
+		'no-duplicate-case': 'error', // 禁止出现重复的 case 标签
+		'no-empty-character-class': 'error', // 禁止在正则表达式中使用空字符集
+		'no-extra-parens': ['error', 'functions'], // 禁止不必要的括号
+		'no-unused-vars': ['error', { vars: 'all', args: 'none' }] // 禁止出现未使用过的变量
+	}
+}
