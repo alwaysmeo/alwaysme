4ec3826affb19e6833bc7e8dde0142acb4f3b993
常伴你左右 <hello2000@vip.qq.com>
2022-12-06 16:32:45 +0800

    update button

--- packages/components/Link/src/link.vue
+++ packages/components/Link/src/link.vue
@@ -1,31 +1,31 @@
 <template>
-    <a
-        :class="[
-            'me-link',
-            {
-                [`me-link-underline`]: props.underline,
-                [`me-link-disabled`]: props.disabled
-            }
-        ]"
-        :href="props.href || 'javascript:;'"
-        :target="props.target"
-        @click="handleClick"
-    >
-        <slot></slot>
-    </a>
+  <a
+    :class="[
+      'me-link',
+      {
+        [`me-link-underline`]: props.underline,
+        [`me-link-disabled`]: props.disabled,
+      },
+    ]"
+    :href="props.href || 'javascript:;'"
+    :target="props.target"
+    @click="handleClick"
+  >
+    <slot></slot>
+  </a>
 </template>
 
 <script setup lang="ts">
-import { LinkProps, Name } from '@components/Link/src/types'
+import { LinkProps, Name } from '@components/Link/src/types';
 
-defineOptions({ name: Name })
+defineOptions({ name: Name });
 
-const props = defineProps(LinkProps)
-const emits = defineEmits(['click'])
+const props = defineProps(LinkProps);
+const emits = defineEmits(['click']);
 
 const handleClick = (event: MouseEvent) => {
-    console.log(props)
-    // if (props.disabled) return event.preventDefault()
-    emits('click', event)
-}
+  console.log(props);
+  // if (props.disabled) return event.preventDefault()
+  emits('click', event);
+};
 </script>
