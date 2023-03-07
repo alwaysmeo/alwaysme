47aa39c1749662d6c9e017219c47068b4f986b32
常伴你左右 <hello2000@vip.qq.com>
2023-03-07 17:16:19 +0800

    update: 完善 button 文档页面

--- document/views/components/Button.vue
+++ document/views/components/Button.vue
@@ -1,11 +1,64 @@
 <template>
-	<div class="container"></div>
+	<div class="container">
+		<PageHeader title="Button 按钮" desc="按钮用于开始一个即时操作" />
+	</div>
 </template>
 
 <script setup lang="ts">
-	import { ref } from 'vue'
-	// import { PageHeader } from '@components'
+	import { onMounted } from 'vue'
+	import { PageHeader } from '@components'
 
+	const code = `
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import prismjs from 'vite-plugin-prismjs'
+import { resolve } from 'path'
+
+export default defineConfig(({ command, mode }) => {
+	const { VITE_HOST, VITE_PORT, VITE_OPEN, VITE_HMR, VITE_OUTDIR } = loadEnv(mode, __dirname)
+
+	return {
+		plugins: [
+			vue(),
+			vueJsx(),
+			prismjs({
+				languages: ['js', 'ts', 'html', 'css'],
+				plugins: ['line-numbers'],
+				theme: 'solarizedlight',
+				css: true
+			})
+		],
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: @import './assets/style/global.scss';
+				}
+			}
+		},
+		resolve: {
+			alias: {
+				'@': resolve(__dirname, './'),
+				'@views': resolve(__dirname, './views'),
+				'@assets': resolve(__dirname, './assets'),
+				'@components': resolve(__dirname, './components'),
+				'@hooks': resolve(__dirname, './hooks')
+			}
+		},
+		build: {
+			outDir: VITE_OUTDIR,
+			chunkSizeWarningLimit: 500,
+			minify: true, // 压缩
+			cssCodeSplit: false
+		},
+		server: {
+			host: VITE_HOST as string,
+			port: VITE_PORT as unknown as number,
+			open: VITE_OPEN as unknown as boolean,
+			hmr: VITE_HMR as unknown as boolean
+		}
+	}
+})`
 </script>
 
 <style scoped lang="scss"></style>
