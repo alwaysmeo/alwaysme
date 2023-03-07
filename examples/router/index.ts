5515c79d15c7a0ce5ece962ded2068765f8bd04f
常伴你左右 <hello2000@vip.qq.com>
2023-03-07 10:47:20 +0800

    update: 优化导入名称

--- examples/router/index.ts
+++ examples/router/index.ts
@@ -2,7 +2,7 @@
 
 import { createRouter, createWebHistory } from 'vue-router'
 import Middleware from '../middleware'
-import components from './components'
+import componentsRouter from './components'
 
 const router = createRouter({
 	history: createWebHistory(),
@@ -19,7 +19,7 @@ const router = createRouter({
 				title: 'Home'
 			}
 		},
-		...components,
+		...componentsRouter,
 		{
 			path: '/:catchAll(.*)',
 			name: 'NotFound',
