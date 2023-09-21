9faf1792989b6d44568acb5bf477acc7a5914080
always <hello2000@vip.qq.com>
2023-09-21 16:16:43 +0800

    delete: 删除 Menu 组件

--- design/packages/components/Menu/types.ts
+++
@@ -1,8 +0,0 @@
-import { ComputedRef } from 'vue'
-
-export interface MenuProvider {
-	mode: ComputedRef<'horizontal' | 'vertical' | undefined>
-	collapse: ComputedRef<boolean>
-	value: ComputedRef<string>
-	handleSelect: (value: string) => void
-}
